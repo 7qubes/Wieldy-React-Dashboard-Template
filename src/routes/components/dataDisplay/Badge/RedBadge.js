d2f1da5301a8e935b35c5f47726b37dced8e66c6
zhicheng <zhicheng22@hotmail.com>
2020-08-18 00:00:38 +0800

    init

---
+++ src/routes/components/dataDisplay/Badge/RedBadge.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Badge, Card} from "antd";
+import Icon from '@ant-design/icons';
+import NotificationOutlined from "@ant-design/icons/lib/icons/NotificationOutlined";
+
+const RedBadge = () => {
+  return (
+    <Card className="gx-card" title="Red Badge">
+      <Badge dot>
+        <NotificationOutlined />
+      </Badge>
+      <Badge count={0} dot>
+        <NotificationOutlined />
+      </Badge>
+      <Badge dot>
+        <a href="/">Link something</a>
+      </Badge>
+    </Card>
+  );
+};
+
+export default RedBadge;
