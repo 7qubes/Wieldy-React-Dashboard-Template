1614da1532329787eb8e3134aff2b0cb8f432ac4
iotfan90 <iotfan90@gmail.com>
2020-09-30 02:13:54 +0800

    Change checkbox

--- src/components/profile/employee/Training/index.js
+++ src/components/profile/employee/Training/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Widget from "components/Widget/index";
-import {Form, Input, Col, Row} from "antd";
+import {Form, Input, Col, Row, Checkbox} from "antd";
 
 const FormItem = Form.Item
 
@@ -12,9 +12,9 @@ const Training = () => {
         <span className="ant-card-head-title gx-mb-1">Training</span>
       </div>
       <div className="ant-card-body">
-        <Row><i className="icon icon-check-square-o"/><p style={{marginLeft: '15px'}}>CPR 01/05/2020</p></Row>
-        <Row><i className="icon icon-check-square-o"/><p style={{marginLeft: '15px'}}>Compliance 01/05/2020</p></Row>
-        <Row><i className="icon icon-check-square-o"/><p style={{marginLeft: '15px'}}>New hire 01/05/2020</p></Row>
+        <Row><Checkbox>CPR 01/05/2020</Checkbox></Row>
+        <Row><Checkbox>Compliance 01/05/2020</Checkbox></Row>
+        <Row><Checkbox>New hire 01/05/2020</Checkbox></Row>
       </div>
     </Widget>
   );
