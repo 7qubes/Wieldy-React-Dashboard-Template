c46666f3e86664ea8738eaa6d5c4a5efe69de78a
kaushikprasanth <kaushikprasanth94@yahoo.com>
2020-11-16 12:22:55 -0600

    Follower Graph addded, Demographics changes

--- src/routes/socialMedia/Analytics/ProfileInteractions.js
+++ src/routes/socialMedia/Analytics/ProfileInteractions.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Card } from "antd";
-import FollowerGraph from "../../../components/SocialMedia/FollowerGraph";
+import BarCharts from "../../../components/SocialMedia/BarCharts";
+
 const ProfileInteractions = (props) => (
     <div>
-        {/* <Card className="gx-card" title="Followers">
-            <FollowerGraph />
+          <Card className="gx-card" title="Page Views">
+            <BarCharts data={props.bar_data} />
         </Card>
-        <Card className="gx-card" title="Followers">
-            <FollowerGraph />
-        </Card> */}
+        <Card className="gx-card" title="Page Likes">
+            <BarCharts data={props.page_likes} />
+        </Card>
     </div>
 )
 
