425941b702722256397f6b1311a7bf58aacd5a7e
Karan Gujrati <kgujra2@uic.edu>
2021-03-12 04:41:46 -0600

    Developed BOM Dashboard Cards
    All the individual cards are developed using bar, line and line-indicator graphs.
    The cards are not arranged on the Home.js component.

--- src/components/Inventories/BOM/Dashboard/Home.js
+++ src/components/Inventories/BOM/Dashboard/Home.js
@@ -4,12 +4,20 @@ import TopLevelPanel from "./TopLevelPanel";
 import TodayPanel from "./TodayPanel";
 import CardPanel from "./CardPanel";
 
+// just for editing
+// import CostByPartsCard from "./GraphCardsColumn/CostByPartsCard";
+// import FinishedItemOverTimeCard from "./GraphCardsColumn/FinishedItemOverTimeCard";
+// import LineCardOne from "./LineIndicatorCardsColumn/LineCardOne";
+// import LineCardTwo from "./LineIndicatorCardsColumn/LineCardTwo";
+// import LineCardThree from "./LineIndicatorCardsColumn/LineCardThree";
+
 const Home = () => {
   return (
     <div>
       <TopLevelPanel />
       <TodayPanel />
       <CardPanel />
+      <div style={{ height: "5vh" }}></div>
     </div>
   );
 };
---
+++ src/components/Inventories/BOM/Dashboard/LineIndicatorCardsColumn/LineCardThree.js
@@ -0,0 +1,66 @@
+import React from "react";
+import LineIndicator from "./LineIndicator";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const LineCardThree = () => {
+  return (
+    <div>
+      <CardBody className="gx-site-dash gx-mb-2 gx-pt-3 gx-pb-3 gx-pt-xl-2">
+        <CardList className="gx-line-indicator">
+          <ListItem>
+            <h6 className="gx-text-uppercase">ESTIMATED LEAD HOURS</h6>
+            <LineIndicator
+              width="78%"
+              title="SUM"
+              secondTitle="10 HOURS"
+              color="#038FDE"
+              value="78%"
+            />
+          </ListItem>
+          <LineBreaker className="line-breaker"></LineBreaker>
+          <ListItem>
+            <h6 className="gx-text-uppercase">ACTUAL LABOR HOURS</h6>
+            <LineIndicator
+              width="18%"
+              title="SUM"
+              secondTitle="8 HOURS"
+              color="#EB2F96"
+              value="18%"
+            />
+          </ListItem>
+        </CardList>
+      </CardBody>
+    </div>
+  );
+};
+
+const CardBody = styled(motion.div)`
+  background: white;
+  font-family: "Poppins", sans-serif;
+  margin-left: 20px;
+  box-shadow: 1px 1px 8px #c9c9c9;
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
+const CardList = styled(motion.div)`
+  margin: 10px;
+  /* padding-left: 50px; */
+`;
+
+const LineBreaker = styled(motion.div)`
+  width: 100%;
+  height: 2px;
+  background: #d9d9d9;
+  margin: 20px 0;
+`;
+
+const ListItem = styled(motion.div)`
+  padding-left: 30px;
+  h6 {
+    padding-bottom: 2px;
+  }
+`;
+
+export default LineCardThree;
